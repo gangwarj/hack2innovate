1d9eda9a3e79f2fd24ed45bad37b15666550bb76
root <root@crudmonks.5ajdverb4l4ungvz3zgswfohbe.jx.internal.cloudapp.net>
2017-11-18 13:25:23 +0000

    asdasf

--- test.py
+++ test.py
@@ -1,55 +1,43 @@
-import numpy as np
-import cv2
-from PIL import Image
-img = Image.open('test.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray,(5,5),0)
-    ret,thresh1 = cv2.threshold(blur,70,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-  
-    _, contours, hierarchy = cv2.findContours(thresh1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    drawing = np.zeros(img.shape,np.uint8)
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-    max_area=0
-   
-    for i in range(len(contours)):
-            cnt=contours[i]
-            area = cv2.contourArea(cnt)
-            if(area>max_area):
-                max_area=area
-                ci=i
-    cnt=contours[ci]
-    hull = cv2.convexHull(cnt)
-    prev_hull = cv2.convexHull(cnt)
-    prev_cnt = cnt
-    moments = cv2.moments(cnt)
-    if moments['m00']!=0:
-                cx = int(moments['m10']/moments['m00']) # cx = M10/M00
-                cy = int(moments['m01']/moments['m00']) # cy = M01/M00
-              
-    centr=(cx,cy)
-    cv2.circle(img,centr,5,[0,0,255],2)       
-    cv2.drawContours(drawing,[cnt],0,(0,255,0),2) 
-    cv2.drawContours(drawing,[hull],0,(255,0,255),2) 
-          
-    cnt = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-    hull = cv2.convexHull(cnt,returnPoints = False)
-    
-    if(1):
-               defects = cv2.convexityDefects(cnt,hull)
-               mind=0
-               maxd=0
-               for i in range(defects.shape[0]):
-                    s,e,f,d = defects[i,0]
-                    start = tuple(cnt[s][0])
-                    end = tuple(cnt[e][0])
-                    far = tuple(cnt[f][0])
-                    dist = cv2.pointPolygonTest(cnt,centr,True)
-                    cv2.line(img,start,end,[0,255,0],2)
-                    cv2.circle(img,far,5,[0,0,255],-1)
-               #print "i=",i,"area=",area,"hull",hull,"prev_hull",prev_hull
-               #print "Points=",prev_hull
-               i=0
-    #change_image[hull] = Clear[hull]
-    #cv2.imshow('final_game',change_image)
-    #cv2.imshow('output',drawing)
-    cv2.imwrite('inputq.jpg',img)
+"""Binarize (make it black and white) an image with Pyhton."""
+
+from PIL import Image
+from scipy.misc import imsave
+import numpy
+
+
+def binarize_image(img_path, target_path, threshold):
+    """Binarize an image."""
+    image_file = Image.open(img_path)
+    image = image_file.convert('L')  # convert image to monochrome
+    image = numpy.array(image)
+    image = binarize_array(image, threshold)
+    imsave(target_path, image)
+
+
+def binarize_array(numpy_array, threshold=200):
+    """Binarize a numpy array."""
+    for i in range(len(numpy_array)):
+        for j in range(len(numpy_array[0])):
+            if numpy_array[i][j] > threshold:
+                numpy_array[i][j] = 255
+            else:
+                numpy_array[i][j] = 0
+    return numpy_array
+
+
+def get_parser():
+    """Get parser object for script xy.py."""
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    parser = ArgumentParser(description=__doc__,formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-i", "--input",dest="input", help="read this file",metavar="FILE", required=True)
+    parser.add_argument("-o", "--output",dest="output",help="write binarized file hre",metavar="FILE",required=True)
+    parser.add_argument("--threshold",dest="threshold", default=200,type=int,help="Threshold when to show white")
+    return parser
+
+
+if __name__ == "__main__":
+    args = get_parser().parse_args()
+    binarize_image(args.input, args.output, args.threshold)
