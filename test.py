9918e627f7e8f4f47e2e8c95902210c804c2c4a7
root <root@crudmonks.5ajdverb4l4ungvz3zgswfohbe.jx.internal.cloudapp.net>
2017-11-18 15:52:38 +0000

    ewihkdjsfs

--- test.py
+++ test.py
@@ -3,17 +3,42 @@
 # -*- coding: utf-8 -*-
 
 """Binarize (make it black and white) an image with Pyhton."""
+#import Image, colorsys
 import colorsys
 from PIL import Image
 from scipy.misc import imsave
 import numpy
 import colorsys
 import cv2
-image = cv2.imread('test.jpg')
-image_hsv = colorsys.rgb_to_hsv(0.2, 0.4, 0.4)
-cv2.imwrite('qwertygf.jpg',image_hsv)
-"""
+def HSVColor(img):
+    if isinstance(img,Image.Image):
+        r,g,b = img.split()
+        Hdat = []
+        Sdat = []
+        Vdat = [] 
+        for rd,gn,bl in zip(r.getdata(),g.getdata(),b.getdata()) :
+            h,s,v = colorsys.rgb_to_hsv(rd/255.,gn/255.,bl/255.)
+            Hdat.append(int(h*255.))
+            Sdat.append(int(s*255.))
+            Vdat.append(int(v*255.))
+        r.putdata(Hdat)
+        g.putdata(Sdat)
+        b.putdata(Vdat)
+        return Image.merge('RGB',(r,g,b))
+    else:
+        return None
 
+a = Image.open('test.jpg')
+b = HSVColor(a)
+b.save('b.jpg')
+
+"""
+image = cv2.imread('test.jpg')
+image_hsv = colorsys.rgb_to_hsv(image)
+cv2.imwrite('dahgsgf.jpg',image_hsv)
+print(image_hsv)
+"""
+"""
 def binarize_image(img_path, target_path, threshold):
     image_file = Image.open(img_path)
     image = colorsys.rgb_to_hsv(359,1,1)
