dd414ebc9a02638d06ce2dddc48397c220a097a0
root <root@crudmonks.5ajdverb4l4ungvz3zgswfohbe.jx.internal.cloudapp.net>
2017-11-18 10:54:24 +0000

    gfd

--- testing.py
+++ testing.py
@@ -1,4 +1,19 @@
+
+#import cv2
+#import numpy as np
+#import copy
+#import math
+#from appscript import app
 from PIL import Image
-im = Image.open('./A-train0001.ppm')
-im.save('test.jpg')
+frame = Image.open('./A-train0001.ppm').convert("L")
+
+frame.save('test1.jpg')
+#bgModel = cv2.BackgroundSubtractorMOG2(0, bgSubThreshold)
+#fgmask = bgModel.apply(frame)
+#res = cv2.bitwise_and(frame, frame, mask=fgmask)
+
+
+
+
 #im.shape
+
